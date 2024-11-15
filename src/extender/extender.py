5f160a352974b24fa65a09af78edae96bdde3521
Kingsley-Yoimiya <kingsley_yoimiya@outlook.com>
2024-11-15 10:17:21 +0800

    add TensorNode as the map's tensor node

--- src/extender/extender.py
+++ src/extender/extender.py
@@ -21,6 +21,41 @@ from ..third_party.utils.protolib import encodeMessage as encode_message
 from ..converter.pytorch_node import PyTorchNode, PyTorchNodeType, PyTorchTensor
 from ..converter.pytorch_converter import PyTorchConverter
 
+class TensorNode:
+    """
+    Represents a tensor node in a PyTorch execution trace, initialized based on each tensor generation.
+    Attributes:
+
+    id (int):               Identifier of the node.
+    extra_id(int):          The Identifier of the node[id]'s copy. (0 means the origin node)
+    value(PyTorchTensor):   The value of the tensor node which is initialized by PyTorchTensor 
+                            with tensor_data (List[int]): Data of the tensor including tensor_id, storage_id, offset, number of elements, and
+                            size of each element in bytes.
+    shape(List[int]):       The shape of the tensor.
+    type(str):              The type of the tensor in chakra node.
+    son(List[int]):         The list of future chakra node will use this node as input to operation.
+    parent(int):            The chakra node that generate this node.
+    """
+    def __init__(self, id, extra_id, value, shape, type):
+        self.id = id
+        self.extra_id = extra_id
+        self.value = PyTorchTensor(value)
+        self.shape = shape
+        self.type = type
+        self.son = []
+        self.parent = -1
+    
+    def add_son(self, x):
+        """
+        Add a son x, which means this node will be use by x as input in the future.
+        """
+        self.son.append(x)
+    
+    def set_parent(self, x):
+        """
+        Set parent, which represents the chakra node generate this node.
+        """
+        self.parent = x
 
 class TraceMap:
     def __init__(
