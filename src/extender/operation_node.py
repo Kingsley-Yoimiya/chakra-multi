29fd3e40474091998b04e8f9d419335845a8861b
Kingsley-Yoimiya <kingsley_yoimiya@outlook.com>
2024-11-18 13:40:43 +0800

    add descriptions of operation_node.py

--- src/extender/operation_node.py
+++ src/extender/operation_node.py
@@ -5,17 +5,23 @@ class OperationNode(PyTorchNode):
     """
     Represents an operation node in a PyTorch execution trace which is initilized by a PyTorch node in chakra.
     And is combined with more information.
-    Attributes:
 
-    old_node(PyTorchNode):      Represents the PyTorchNode.
-    extra_node(int):            Represents the ctrl deps sons which should be ignored.
-    ignore(bool):               If the PyTorchNode have ctrl deps which is not the root, then it should be ignored and processed in the post_process.
-    input_ids(List[int]):       Relabeled tensors of the PyTorchNode's inputs.
-    output_ids(List[int]):      Relabeled tensors of the PyTorchNode's outputs.
-    Other Info from old_node
+    Attributes:
+        old_node(PyTorchNode):      Represents the PyTorchNode.
+        extra_node(int):            Represents the ctrl deps sons which should be ignored.
+        ignore(bool):               If the PyTorchNode have ctrl deps which is not the root, then it should be ignored and processed in the post_process.
+        input_ids(List[int]):       Relabeled tensors of the PyTorchNode's inputs.
+        output_ids(List[int]):      Relabeled tensors of the PyTorchNode's outputs.
+        Other Info from old_node
     """
 
-    def __init__(self, old_node: PyTorchNode):
+    def __init__(self, old_node: PyTorchNode) -> None:
+        """
+        Initialize an operation node with PytorchNode called old_node.
+
+        Args:
+            old_node(PyTorchNode):  The node of the PyTorchNode.
+        """
         self.__dict__.update(old_node.__dict__)
         self.old_node = old_node
         self.extra_node = []
